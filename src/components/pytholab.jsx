54cb00150d3dbd5491d964ab0eb8c026051784cc
Dipu Kumar <dipukumarpojhi@gmail.com>
2026-04-19 10:45:46 +0530

    modified card and navigate to new page

---
+++ src/components/pytholab.jsx
@@ -0,0 +1,13 @@
+function Physiotherapy() {
+  return (
+    <div style={{ padding: "50px", textAlign: "center" }}>
+      <h1>Physiotherapy Treatment</h1>
+      <p>
+        We provide advanced rehabilitation, exercise therapy, pain relief,
+        posture correction and recovery treatment.
+      </p>
+    </div>
+  );
+}
+
+export default Physiotherapy;
